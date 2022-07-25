f8a5929bb6e54fdb5d462ae3c5247b10f1e8e55a
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2022-07-25 14:54:09 +0200

    fixed circular dependencies

--- timecourse_analysis/__main__.py
+++ timecourse_analysis/__main__.py
@@ -1,20 +1,26 @@
 import sys 
 import preprocessor
 from initialize_preprocess import *
-from diffusion import all_diffusions
-from interactions import all_interactions
-from spreadingrate import all_spreading_rates
+
+
+def preprocess(): 
+    preprocessor.preprocess_data(0, deuxcvsixtweets, deuxcvsixfollower, os.path.join("impressions", "deuxcvsix_impressions.csv"))
+    preprocessor.preprocess_data(1, docknacktweets, docknackfollower, os.path.join("impressions", "docknack_impressions.csv"))
+    preprocessor.preprocess_data(3, florianaignertweets, florianaignerfollower, os.path.join("impressions", "florianaigner_impressions.csv"))
+    preprocessor.preprocess_data(4, lassehallstroemtweets, lassehallstroemfollower, os.path.join("impressions", "lassehallstroem_impressions.csv"))
+    preprocessor.preprocess_data(5, rosenbuschtweets, rosenbuschfollower, os.path.join("impressions", "rosenbusch_impressions.csv"))
+    preprocessor.preprocess_data(0, themiostweets, themiosfollower, os.path.join("impressions", "themios_impressions.csv"))
+
+def statistics(): 
+    import diffusion 
+    import interactions 
+    import spreadingrate 
+    all_diffusions()
+    all_interactions()
+    all_spreading_rates()
+
 
 data_directory = sys.argv[1]
 os.chdir(data_directory) # directory with queried follower - files
-
-preprocessor.preprocess_data(0, deuxcvsixtweets, deuxcvsixfollower, os.path.join("impressions", "deuxcvsix_impressions.csv"))
-preprocessor.preprocess_data(1, docknacktweets, docknackfollower, os.path.join("impressions", "docknack_impressions.csv"))
-preprocessor.preprocess_data(3, florianaignertweets, florianaignerfollower, os.path.join("impressions", "florianaigner_impressions.csv"))
-preprocessor.preprocess_data(4, lassehallstroemtweets, lassehallstroemfollower, os.path.join("impressions", "lassehallstroem_impressions.csv"))
-preprocessor.preprocess_data(5, rosenbuschtweets, rosenbuschfollower, os.path.join("impressions", "rosenbusch_impressions.csv"))
-preprocessor.preprocess_data(0, themiostweets, themiosfollower, os.path.join("impressions", "themios_impressions.csv"))
-
-all_diffusions()
-all_interactions()
-all_spreading_rates()
+preprocess()
+statistics()
